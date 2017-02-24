77858ce395b14f9146a4acca1fe5b706b14c648e
Edward Park <ed.sh.park@gmail.com>
2017-02-24 12:37:50 -0800

    entry point (.js -> .jsx)

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -12,4 +12,16 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'));*/
+
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import Root from './containers/Root.jsx';
+import store from './store/index';
+
+render(
+  <Root store={store} />,
+  document.querySelector('.container')
+);
