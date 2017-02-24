550e504c6f5e43db89ccf006af15c733a4e6b4ef
Edward Park <ed.sh.park@gmail.com>
2017-02-24 12:38:47 -0800

    commenting out index.js reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,85 @@
-import { combineReducers } from 'redux';
+// ----------------------------------------
+// -------- final Refactor ----------------
+// ----------------------------------------
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+// import { combineReducers } from 'redux';
+// import * as reducers from './reducers';
 
-export default rootReducer;
+// const todoApp = combineReducers(reducers);
+
+// export default todoApp;
+
+
+// ----------------------------------------
+// ---- using combineReducers ------------
+// ----------------------------------------
+
+// import visibilityFilter from './visibilityReducer';
+// import todos from './todoReducer';
+
+// const todoApp = combineReducers({
+//   visibilityFilter,
+//   todos
+// })
+
+// export default todoApp;
+
+
+// ----------------------------------------
+// ---------- MANUAL WAY ------------------
+// ----------------------------------------
+
+// -------- initialization can happen in the lower reducers -------
+// ----------------- as default parameters ----------------
+
+// import { VisibilityFilters } from '../actions';
+// const { SHOW_ALL } = VisibilityFilters;
+
+// const initialState = {
+//   visibilityFilter = SHOW_ALL,
+//   todos: []
+// };
+
+// const todoApp = (state = initialState, action) => {
+  // switch (action.type) {
+  //   case SET_VISIBILITY_FILTER:
+  //     return {
+  //       ...state,
+  //       visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+  //     };
+
+  //   case ADD_TODO:
+  //   case TOGGLE_TODO:
+  //     return {
+  //       ...state,
+  //       todos: todos(state.todos, action)
+  //     };
+
+    // case ADD_TODO:
+    //   return {
+    //     ...state,
+    //     todos: [
+    //       ...state.todos,
+    //       { text: action.text, completed: false }
+    //     ]
+    //   };
+    // case TOGGLE_TODO:
+    //   return {
+    //     ...state, todos: state.todos.map((todo, i) => { // mapping through previous 'todos' array, changing the 'completed' boolean status
+    //       if (i === action.index) {
+    //         return { ...todo, completed: !todo.completed }; // toggling the 'completed' boolean back and forth
+    //       } else {
+    //         return todo;
+    //       }
+    //     })
+    //   };
+
+  //   default:
+  //     return state;
+  // }
+
+//   return { 
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     todos: todos(state.todos, action),
+//   }
+// }
